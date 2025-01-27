8542f9f6faeba4b5f73fa34ed41d3738b1680e5a
yousefdewidar <ymahmoud1213@gmail.com>
2025-01-27 18:41:41 +0200

    Remove Hive now

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:al_muslim/main_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const AlMuslim());
 }
